4bb3d1101edca1a55615334c96f632747ffa0bf3
austin <aclyde11@gmail.com>
2019-04-29 16:45:32 -0500

    added embedding lyaer

--- preprocessor.py
+++ preprocessor.py
@@ -3,7 +3,7 @@ from featurizer import OneHotFeaturizer
 from tqdm import tqdm
 
 from optparse import OptionParser
-
+import pickle
 
 def convert_to_embed(smi):
     return np.array([vocab[x] for x in smi])
@@ -16,24 +16,36 @@ if __name__ == '__main__':
     parser.add_option("-t", "--train", dest="train_path")
     parser.add_option("-n", "--split", dest="nsplits", default=10)
     parser.add_option("-j", "--jobs", dest="njobs", default=8)
+    parser.add_option("-o", "--out", dest="out", default="out")
+    parser.add_option("-v", "--vocab", dest='vocab', default=None)
+    parser.add_option("-m", "--max_len", dest='max_len', default=None)
     opts, args = parser.parse_args()
     opts.njobs = int(opts.njobs)
 
     vocab = set()
     max_len = 0
     smiles = []
-    with open(opts.train_path) as f:
-        for smi in tqdm(f):
-            smi = smi.rstrip()
-            smiles.append(smi)
-            max_len = max(len(smi), max_len)
-            for i in smi:
-                vocab.add(i)
 
-    vocab.add(' ')
-    vocab = list(vocab)
-    vocab = { c : v for v, c in enumerate(vocab)}
+    if opts.vocab is None:
+        with open(opts.train_path) as f:
+            for smi in tqdm(f):
+                smi = smi.rstrip()
+                smiles.append(smi)
+                max_len = max(len(smi), max_len)
+                for i in smi:
+                    vocab.add(i)
 
+        vocab.add(' ')
+        vocab = list(vocab)
+        vocab = { c : v for v, c in enumerate(vocab)}
+
+
+        with open(opts.out + "vocab.pkl", 'w') as f:
+            pickle.dump(vocab, f)
+    else:
+        with open(opts.vocab, 'r') as f:
+            vocab = pickle.load(f)
+            max_len = opts.max_len
 
 
     #ohf = OneHotFeaturizer(vocab=vocab, padlength=max_len + 5)
