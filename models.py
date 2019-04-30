1f10c9f78c1f954d56a3c3c03f85da1cba868fed
austin <aclyde11@gmail.com>
2019-04-29 20:48:54 -0500

    added embedding lyaer

--- models.py
+++ models.py
@@ -5,20 +5,56 @@ import torch.utils.data
 from torch import nn, optim
 from torch.nn import functional as F
 
+
+
+class TimeDistributed(nn.Module):
+    def __init__(self, module, batch_first=True):
+        super(TimeDistributed, self).__init__()
+        self.module = module
+        self.batch_first = batch_first
+
+    def forward(self, x):
+
+        if len(x.size()) <= 2:
+            return self.module(x)
+
+        # Squash samples and timesteps into a single axis
+        x_reshape = x.contiguous().view(-1, x.size(-1))  # (samples * timesteps, input_size)
+
+        y = self.module(x_reshape)
+
+        # We have to reshape Y
+        if self.batch_first:
+            y = y.contiguous().view(x.size(0), -1, y.size(-1))  # (samples, timesteps, output_size)
+        else:
+            y = y.view(-1, x.size(1), y.size(-1))  # (timesteps, samples, output_size)
+
+        return y
+
 class MolecularVAE(nn.Module):
-    def __init__(self):
+    def __init__(self, max_len, word_embedding_size, vocab_size, latent_size=292):
         super(MolecularVAE, self).__init__()
 
-        self.conv1d1 = nn.Conv1d(120, 9, kernel_size=9)
+        self.max_len = max_len
+        self.word_embedding_size = word_embedding_size
+        self.vocab_size = self.vocab_size
+
+        self.embedding = nn.Embedding(vocab_size, word_embedding_size)
+        self.linear = TimeDistributed(nn.Linear(word_embedding_size, vocab_size))
+
+        self.conv1d1 = nn.Conv1d(word_embedding_size, 9, kernel_size=20)
         self.conv1d2 = nn.Conv1d(9, 9, kernel_size=9)
         self.conv1d3 = nn.Conv1d(9, 10, kernel_size=11)
-        self.fc0 = nn.Linear(90, 435)
-        self.fc11 = nn.Linear(435, 292)
-        self.fc12 = nn.Linear(435, 292)
+        self.conv1d3 = nn.Conv1d(10, 10, kernel_size=10)
+        self.conv1d3 = nn.Conv1d(10, 10, kernel_size=10)
 
-        self.fc2 = nn.Linear(292, 292)
-        self.gru = nn.GRU(292, 501, 3, batch_first=True)
-        self.fc3 = nn.Linear(501, 35)
+        self.fc0 = nn.Linear(195, 435)
+        self.fc11 = nn.Linear(435, latent_size)
+        self.fc12 = nn.Linear(435, latent_size)
+
+        self.fc2 = nn.Linear(latent_size, latent_size)
+        self.gru = nn.GRU(latent_size, 501, 3, batch_first=True)
+
 
     def encode(self, x):
         h = F.relu(self.conv1d1(x))
@@ -39,12 +75,10 @@ class MolecularVAE(nn.Module):
 
     def decode(self, z):
         z = F.selu(self.fc2(z))
-        z = z.view(z.size(0), 1, z.size(-1)).repeat(1, 120, 1)
-        out, h = self.gru(z)
-        out_reshape = out.contiguous().view(-1, out.size(-1))
-        y0 = F.softmax(self.fc3(out_reshape), dim=1)
-        y = y0.contiguous().view(out.size(0), -1, y0.size(-1))
-        return y
+        z = z.view(z.size(0), 1, z.size(-1)).repeat(1, self.max_len, 1)
+        out, _ = self.gru(z)
+        out = self.linear(out)
+        return out
 
     def forward(self, x):
         mu, logvar = self.encode(x)
