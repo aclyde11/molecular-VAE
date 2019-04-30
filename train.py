88e22cfd0ef394c819e21496655b586937f0f9b8
austin <aclyde11@gmail.com>
2019-04-30 06:00:45 -0500

    added embedding lyaer

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ df_train = df[~msk]
 df_test = df[~msk]
 
 max_len = 255
-vocab = pickle.load(open("outvocab.pkl", 'wb'))
+vocab = pickle.load(open("outvocab.pkl", 'rb'))
 
 train_dataset = MoleLoader(df_train, vocab, max_len)
 test_dataset  = MoleLoader(df_test, vocab, max_len)
