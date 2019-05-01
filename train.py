2282e2bd17cd0ea84ef00d19f67829748ecb10f2
austin <aclyde11@gmail.com>
2019-04-30 20:50:49 -0500

    added embedding lyaer

--- train.py
+++ train.py
@@ -17,9 +17,9 @@ def onehot_initialization_v2(a):
     return out
 
 def loss_function(recon_x, x, mu, logvar):
-    x = x.numpy()
+    x = x.cpu().numpy()
     x = onehot_initialization_v2(x)
-    x = torch.from_numpy(x).float()
+    x = torch.from_numpy(x).gpu().float()
     print(x.shape)
     BCE = F.cross_entropy(recon_x, x, size_average=False)
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
