90afe03abcf4b737d22762271732bd7fc6ac7327
Austin Clyde <aclyde@uchicago.edu>
2019-05-04 08:24:01 -0500

    updated b sizw

--- data_loader.py
+++ data_loader.py
@@ -1,7 +1,7 @@
 import torch
 from torchvision import datasets, transforms
 import numpy as np
-
+from sklearn.preprocessing import OneHotEncoder
 
 class MoleLoader(torch.utils.data.Dataset):
     def __init__(self, df, vocab, max_len=70, num=None):
@@ -10,17 +10,27 @@ class MoleLoader(torch.utils.data.Dataset):
         self.df = df
         self.vocab = vocab
         self.max_len = max_len
-
+        self.one_hot_encoder =  OneHotEncoder(categories=list(range(len(vocab))) ,handle_unknown='error')
 
 
 
     def __len__(self):
         return self.df.shape[0]
 
+    def one_hot_encode(self, item):
+        stacks = []
+        for i in item:
+            print(i)
+            tmp = self.one_hot_encoder.fit(i)
+            print(tmp.shape)
+            stacks.append(tmp)
+        s = np.stack(stacks)
+        print(s.shape)
+        return s
 
 
     def __getitem__(self, item):
         smile = str(self.df.iloc[item, 0]).ljust(self.max_len, ' ')
         embedding = np.array([self.vocab[x] for x in smile])
-        embedding = torch.LongTensor(embedding)
+        embedding = self.one_hot_encode(embedding)
         return embedding
