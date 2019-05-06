cfb2ca2728af9fe5ec8a4c3fead4ce14a3abef7d
austin <aclyde11@gmail.com>
2019-05-06 07:39:24 -0500

    added embedding lyaer

--- models.py
+++ models.py
@@ -114,7 +114,7 @@ class MolEncoder(nn.Module):
         self.i = i
         self.embedding = nn.Embedding(num_embeddings=c, embedding_dim=word_embedding_size)
 
-        self.gru = nn.LSTM(word_embedding_size, 128, 2, batch_first=True)
+        self.gru = nn.LSTM(word_embedding_size, 128, 3, batch_first=True)
         self.conv_1 = ConvSELU(i, 15, kernel_size=9)
         self.conv_2 = ConvSELU(15, 15, kernel_size=9)
         self.conv_3 = ConvSELU(15, 15, kernel_size=11)
