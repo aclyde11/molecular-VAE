d1fed7d17a3094b8c76c4eea52d681e39004ab13
Austin Clyde <aclyde@uchicago.edu>
2019-05-07 15:45:29 -0500

    updated b sizw

--- train.py
+++ train.py
@@ -31,7 +31,7 @@ def loss_function(recon_x, x, mu, logvar):
 
 
 
-df = pd.read_csv("zinc_cleaned_cannon.smi", header=None)
+df = pd.read_csv("/vol/ml/aclyde/ZINC/zinc_cleaned_cannon.smi", header=None)
 df = df.iloc[0:2000000,:]
 max_len = 125
 
