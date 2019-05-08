9fe68f63e6db60fb2d2e4a19653d9ed958d15acb
Austin Clyde <aclyde@uchicago.edu>
2019-05-08 12:06:27 -0500

    updated b sizw

--- train.py
+++ train.py
@@ -1,24 +1,22 @@
-from comet_ml import Experiment
-
 import numpy as np
+import pandas as pd
 import torch
 import torch.utils.data
+from comet_ml import Experiment
 from torch import nn, optim
-import torch.nn.functional as F
+from tqdm import tqdm
+
 from data_loader import MoleLoader
 from models import MolecularVAE
-import pandas as pd
-import pickle
-from tqdm import tqdm
-from rdkit import Chem
 
 
 def onehot_initialization_v2(a):
     ncols = len(vocab)
-    out = np.zeros( (a.size,ncols), dtype=np.uint8)
-    out[np.arange(a.size),a.ravel()] = 1
+    out = np.zeros((a.size, ncols), dtype=np.uint8)
+    out[np.arange(a.size), a.ravel()] = 1
     out.shape = a.shape + (ncols,)
     return out
+
 
 def loss_function(recon_x, x, mu, logvar):
     recon_x = recon_x.contiguous().view(-1)
@@ -30,16 +28,14 @@ def loss_function(recon_x, x, mu, logvar):
     return xent_loss + kl_loss
 
 
-
 df = pd.read_csv("/vol/ml/aclyde/ZINC/zinc_cleaned_cannon.smi", header=None)
-#df = df.iloc[0:2000000,:]
+# df = df.iloc[0:2000000,:]
 max_len = 128
-
 
 vocab = set()
 bads = []
 for i in tqdm(df.itertuples(index=True)):
-    if len(i[1]) < max_len :
+    if len(i[1]) < max_len:
         for c in i[1]:
             vocab.add(c)
     else:
@@ -50,8 +46,8 @@ df = df.drop(bads, axis=0)
 print(df.head())
 print(df.shape)
 
-vocab = {c : i for i, c in enumerate(list(vocab))}
-charset = {i : c for i, c in enumerate(list(vocab))}
+vocab = {c: i for i, c in enumerate(list(vocab))}
+charset = {i: c for i, c in enumerate(list(vocab))}
 print(vocab)
 msk = np.random.rand(len(df)) < 0.8
 df_train = df[msk]
@@ -62,20 +58,24 @@ print(df_test.shape)
 
 lossf = nn.CrossEntropyLoss()
 train_dataset = MoleLoader(df_train, vocab, max_len)
-test_dataset  = MoleLoader(df_test, vocab, max_len)
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1024, shuffle=True, num_workers = 32, pin_memory = True)
-test_loader  = torch.utils.data.DataLoader(test_dataset, batch_size=1024, shuffle=True, num_workers =  32, pin_memory = True)
+test_dataset = MoleLoader(df_test, vocab, max_len)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=3072, shuffle=True, num_workers=8 * 6,
+                                           pin_memory=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=3072, shuffle=True, num_workers=8 * 6,
+                                          pin_memory=True)
 torch.manual_seed(42)
 
 epochs = 3000
 
 model = MolecularVAE(i=max_len, c=len(vocab), o=512).cuda()
 model = nn.DataParallel(model)
-optimizer = optim.SGD(model.parameters(), lr=3.0e-4)
-scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.9, patience=10, verbose=True, threshold=1e-3)
+optimizer = optim.Adam(model.parameters(), lr=3.0e-4 * 2 * 6)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.9, patience=10, verbose=True,
+                                                 threshold=1e-3)
 log_interval = 100
 
 experirment = Experiment(project_name='pytorch', auto_metric_logging=False)
+
 
 def train(epoch):
     with experirment.train():
@@ -105,6 +105,7 @@ def train(epoch):
         print('train', train_loss / len(train_loader.dataset))
         return train_loss / len(train_loader.dataset)
 
+
 def test(epoch):
     with experirment.test():
         model.eval()
@@ -115,7 +116,7 @@ def test(epoch):
             ohe = ohe.cuda()
 
             recon_batch, mu, logvar = model(data)
-            loss =  loss_function(recon_batch, ohe, mu, logvar)
+            loss = loss_function(recon_batch, ohe, mu, logvar)
             test_loss += loss.item()
             n += 1
             experirment.log_metric('loss', loss.item())
@@ -123,8 +124,8 @@ def test(epoch):
             num_right = 0
             _, preds = torch.max(recon_batch, dim=2)
             for i in range(recon_batch.shape[0]):
-                num_right += int(torch.equal(preds[i,...], data[i,...]))
-            experirment.log_metric('accuracy', float(num_right)/float(recon_batch.shape[0]))
+                num_right += int(torch.equal(preds[i, ...], data[i, ...]))
+            experirment.log_metric('accuracy', float(num_right) / float(recon_batch.shape[0]))
 
             if batch_idx % log_interval == 0:
                 preds = preds.cpu().numpy()
@@ -140,6 +141,7 @@ def test(epoch):
         print('test', test_loss / len(test_loader))
         return float(test_loss) / float(n)
 
+
 for epoch in range(1, epochs + 1):
     experirment.log_current_epoch(epoch)
     train_loss = train(epoch)
@@ -150,10 +152,10 @@ for epoch in range(1, epochs + 1):
     for param_group in optimizer.param_groups:
         lr = param_group['lr']
     experirment.log_metric('lr', lr)
-    torch.save( { 'model_state_dict' : model.module.state_dict(),
-                  'optimizer_state_dict' : optimizer.state_dict(),
-                  'epoch' : epoch,
-                  'charset' : charset,
-                  'max_len' : max_len,
-                  'lr'      : lr
-    }, "save.pt")
+    torch.save({'model_state_dict': model.module.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'epoch': epoch,
+                'charset': charset,
+                'max_len': max_len,
+                'lr': lr
+                }, "save.pt")
