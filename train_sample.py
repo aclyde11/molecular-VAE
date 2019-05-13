c88dc02517c0f715f23d35c4410f06b8d590ba94
Austin Clyde <aclyde@uchicago.edu>
2019-05-13 14:41:04 -0500

    changed to SGD

--- train_sample.py
+++ train_sample.py
@@ -12,7 +12,7 @@ from data_loader import MoleLoader
 from models import MolecularVAE
 import argparse
 import sys
-
+from rdkit import Chem
 checkpoint = torch.load(sys.argv[1], map_location='cpu')
 
 model = MolecularVAE(i=checkpoint['max_len'], c=len(checkpoint['charset']), o=360)
@@ -21,19 +21,25 @@ model = model.cuda()
 charset = checkpoint['charset']
 
 model.eval()
-b_size = 1000
-times = 1000
+b_size = 2000
+max_times = 1e9
+counter = 0
+total =0
 with open(sys.argv[2], 'w') as f:
     with torch.no_grad():
-        for batch_idx in range(times):
-            print(batch_idx)
+        while not (total > max_times or counter > 10000):
             sampler = torch.rand(size=(b_size, 360)).cuda()
             recon_batch = model.decoder(sampler)
             _, preds = torch.max(recon_batch, dim=2)
             preds = preds.cpu().numpy()
-            for i in range(b_size):
+            for i in tqdm(range(b_size)):
                 sample = preds[i, ...]
                 out = "".join([charset[chars] for chars in sample]).rstrip()
-                f.write(out)
-                f.write("\n")
+                m = Chem.SmilesToMol(out)
+                total += 1
+                if m is not None:
+                    counter += 1
+                    print("counter = ", counter)
+                    f.write(out)
+                    f.write("\n")
 
