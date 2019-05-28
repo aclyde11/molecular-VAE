c83840b3bae6e2772f78e2a889c25b0fc9857d45
Austin Clyde <aclyde@uchicago.edu>
2019-05-28 13:36:50 -0500

    data parllel

--- moses_train_distrib.py
+++ moses_train_distrib.py
@@ -143,8 +143,7 @@ def _train_epoch(model, epoch, tqdm_data, kl_weight, optimizer=None):
 
         loss = kl_weight * kl_loss + recon_loss
         print(loss.shape)
-        loss = loss.sum()
-        print(loss.shape)
+        loss = torch.sum(loss, 0)
 
         # Backward
         if optimizer is not None:
