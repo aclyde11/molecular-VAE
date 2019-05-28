d97c45c43cd7ed50bbb5cff65d6e067c1d8581b7
Austin Clyde <aclyde@uchicago.edu>
2019-05-28 14:00:45 -0500

    data parllel

--- moses_train_distrib.py
+++ moses_train_distrib.py
@@ -133,7 +133,7 @@ train_loader = torch.utils.data.DataLoader(df, batch_size=2048,
 n_epochs = 50
 
 model = mosesvae.VAE(vocab)
-model = torch.distributed.DistributedDataParallel(model, device_ids=[i], output_device=i)
+model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[i], output_device=i)
 
 optimizer = optim.Adam((p for p in model.parameters() if p.requires_grad),
                                lr=3*1e-4)
