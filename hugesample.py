2fc64dd25ecf4680a45da348ed6b2ed5f05b2bb5
Austin Clyde <aclyde@uchicago.edu>
2019-06-19 18:46:31 -0500

    training

--- hugesample.py
+++ hugesample.py
@@ -12,30 +12,34 @@ from tqdm import tqdm
 from multiprocessing import Process, Pipe, Queue, Manager, Value
 
 def gen_proc(comm, iters=10000, i=0, batch_size=4096):
-    with open("sym_table.pkl", 'rb') as f:
-        sym_table = pickle.load(f)
-    with open("charset.pkl", 'rb') as f:
-        charset = pickle.load(f)
-    with open("vocab.pkl", 'rb') as f:
-        vocab = pickle.load(f)
-    model = mosesvae.VAE(vocab).cuda()
-    model.load_state_dict(torch.load("trained_save.pt"))
-    model = model.cuda(i)
+    try:
+        with open("sym_table.pkl", 'rb') as f:
+            sym_table = pickle.load(f)
+        with open("charset.pkl", 'rb') as f:
+            charset = pickle.load(f)
+        with open("vocab.pkl", 'rb') as f:
+            vocab = pickle.load(f)
+        model = mosesvae.VAE(vocab).cuda()
+        model.load_state_dict(torch.load("trained_save.pt"))
+        model = model.cuda(i)
 
-    for _ in tqdm(range(iters)):
-        count = 0
+        for _ in tqdm(range(iters)):
+            count = 0
 
-        res, _, _ = model.sample(batch_size)
+            res, _, _ = model.sample(batch_size)
 
-        smis = []
-        for i in range(batch_size):
-            count += 1
-            try:
-                s = selfies.decoder("".join(['[' + charset[sym] + ']' for sym in res[i]]))
-                smis.append(s)
-            except:
-                print("ERROR!!!")
-        comm.put((smis, count))
+            smis = []
+            for i in range(batch_size):
+                count += 1
+                try:
+                    s = selfies.decoder("".join(['[' + charset[sym] + ']' for sym in res[i]]))
+                    smis.append(s)
+                except:
+                    print("ERROR!!!")
+            comm.put((smis, count))
+    except KeyboardInterrupt:
+        print("exiting")
+        exit()
 
 
 def hasher(q, hasher, valid, total, i):
@@ -62,12 +66,16 @@ def hasher(q, hasher, valid, total, i):
                     print("error...")
 
 def reporter(q, d, valid):
-    while True:
-        time.sleep(2)
-        print("Reporting! ")
-        print("Queue length: ",     q.qsize())
-        print("Valid: ", len(d))
-        print("Valid: ", valid.value)
+    try:
+        while True:
+            time.sleep(2)
+            print("Reporting! ")
+            print("Queue length: ",     q.qsize())
+            print("Valid: ", len(d))
+            print("Valid: ", valid.value)
+    except KeyboardInterrupt:
+        print("Exiting")
+        exit()
 
 
 if __name__ == '__main__':
@@ -83,8 +91,13 @@ if __name__ == '__main__':
     p.start()
     h.start()
     r.start()
-    p.join()
-    h.join()
-    r.join()
+    try:
+        p.join()
+        h.join()
+        r.join()
+    except KeyboardInterrupt:
+        p.kill()
+        h.kill()
+        r.kill()
 
 
