bb198d6f87d2943c51c1df988ba8635bf9f65df3
Austin Clyde <aclyde@uchicago.edu>
2019-06-19 18:57:51 -0500

    training

--- hugesample.py
+++ hugesample.py
@@ -79,7 +79,7 @@ def reporter(q, d, valid, total):
                 print("Valid: ", valid.value, float(valid.value)  / total.value)
                 print("Sampled: ", total.value)
                 print("Samples per second: ", float(total.value) / float(start_time - time.time()) )
-                print("Unique per second: ", float(len(d)) / float(start_time - time.time()) )
+                print("Unique per second: ", float(len(d)) / float(time.time() - start_time) )
             except ZeroDivisionError:
                 print("eh zero error.")
 
