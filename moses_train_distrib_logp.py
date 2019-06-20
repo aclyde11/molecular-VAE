975f1b088c18676c15b538f9e22a313ceb4aef64
Austin Clyde <aclyde@uchicago.edu>
2019-06-20 14:51:22 -0500

    training

--- moses_train_distrib_logp.py
+++ moses_train_distrib_logp.py
@@ -267,7 +267,7 @@ def _train_epoch_binding(model, epoch, tqdm_data, kl_weight, optimizer=None):
         kl_loss = torch.sum(kl_loss, 0)
         recon_loss = torch.sum(recon_loss, 0)
 
-        loss = min(1.0, kl_weight) * kl_loss + recon_loss
+        loss = kl_loss + recon_loss
 
 
         # Backward
