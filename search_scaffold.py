0c9f38e45fdd6b52b584f31e31d721363122762c
Austin Clyde <aclyde@uchicago.edu>
2019-06-30 09:23:49 -0400

    training

--- search_scaffold.py
+++ search_scaffold.py
@@ -6,6 +6,14 @@ from oe_analysis import FastRocker
 
 fast_rocs = FastRocker("../data.oeb")
 df = pd.read_csv("finetuning/out_samples.smi", header=None)
-for _, smi in df.iterrows():
-    smi = smi[0]
-    print(fast_rocs.get_color(smi))
+
+with open("log.txt", 'w', buffering=1) as f:
+    f.write("smiles,color,pdb_match,sim\n")
+    for _, smi in df.iterrows():
+        smi = smi[0]
+        x = fast_rocs.get_color(smi)
+        if x is not None:
+            f.write(smi + ",")
+            f.write(str(x[0]) + ",")
+            f.write(str(x[1]) + ",")
+            f.write("0\n")
