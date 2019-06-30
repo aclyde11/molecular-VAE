8194d53f540856a84b359c6b719ff40869b7b96b
Austin Clyde <aclyde@uchicago.edu>
2019-06-30 09:07:25 -0400

    training

--- oe_analysis.py
+++ oe_analysis.py
@@ -47,7 +47,7 @@ class FastRocker():
         ##use omega here to get conformers:
         with open("tmp.smi", 'w') as f:
             f.write(smi + " tmp_0\n")
-        subprocess.check_call(["/workspace/openeye/bin/omega2", "-in tmp.smi -out tmp.oeb.gz"])
+        subprocess.check_call(["/workspace/openeye/bin/omega2", "-in tmp.smi -out tmp.oeb.gz"], shell=True)
         qfname = "tmp.oeb.gz"
 
         # read in query
