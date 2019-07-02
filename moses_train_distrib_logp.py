db9c275b65fbce5f0edaa3daaa72ec5df5592ea6
Austin Clyde <aclyde@uchicago.edu>
2019-07-02 15:30:48 -0400

    training

--- moses_train_distrib_logp.py
+++ moses_train_distrib_logp.py
@@ -341,7 +341,7 @@ def _train_epoch_binding(model, epoch, tqdm_data, kl_weight, iters, rate, encode
             sample = predict[i,...].tolist()
             print(sample)
             print(vocab.i2c[sample[0]])
-            print(selfies.decoder("".join(['[' + charset[vocab.i2c[sample]] + ']' for sym in sample])))
+            print(selfies.decoder("".join(['[' + charset[vocab.i2c[sym]] + ']' for sym in sample])))
         exit()
         # correct = float((x[:, 1:] == predict).sum().cpu().detach().item()) / float(x.shape[0] * x.shape[1])
 
